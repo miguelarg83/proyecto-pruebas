6a0e4b3459027fd8e38591ce1b437b1315588cbf
miguelarg83 <miguelarg83@hotmail.com>
2020-12-21 20:35:06 +0100

    Modelo Category relacion consigo mismo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,21 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+//use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    //$category->categories
-    public function categories()
-    {
-        return $this->hasMany('App\Models\Categoy','parent_id');
-    }
-
     //$category->parent
     public function parent()
     {
         return $this->belongsTo('App\Models\Category');
+    }
+
+    //$category->categories
+    public function categories()
+    {
+        return $this->hasMany('App\Models\Category','parent_id');
     }
 }
